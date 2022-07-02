61b92570e1de1accf1dfc768a6d86796547b22a7
Eneko Rodríguez <eneko.rodriguez@rrverse.com>
2022-07-02 18:45:23 +0200

    chore: add public path

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
 			// rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-			// publicPath: '/',
+			publicPath: '/mestrejs/',
 			// analyze: true,
 			env: {
 				VITE_API_URL: 'http://localhost:8055',
